178deb6e695094076f6855a6f9391b8b8fab622d
regonn <regonn314@gmail.com>
2014-04-13 12:58:51 +0900

    taskコントローラーのバグシュウセイ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
   def create
     @satellite = Satellite.find_by name: params[:SatelliteName]
-    @user = User.find_by(access_token: params[:access_token]).update(satellite_id: @satellite.id)
+    @user = User.find_by(access_token: params[:access_token])
+    @user.update(satellite_id: @satellite.id)
     @user.tasks.create(content: params[:task], start_time: Time.now, user_id: @user.id)
   end
 end
