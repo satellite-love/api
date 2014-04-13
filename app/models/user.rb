d2296770a545bf13316c5ff4a1804afd14290540
regonn <regonn314@gmail.com>
2014-04-13 11:21:33 +0900

    access token bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   require 'digest/sha1'
   has_many :tasks
-  belongs_to :satelite
+  belongs_to :satellite
 
   def make_access_token
-    self.access_token = Digest::SHA1.hexdigest(self.id.to_s)
+    self.update( access_token: Digest::SHA1.hexdigest(self.id.to_s))
     self.save
   end
 end
