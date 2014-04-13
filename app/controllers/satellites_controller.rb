c6089b5baa6054eff15f65deaed6811ab0c76926
regonn <regonn314@gmail.com>
2014-04-13 12:15:03 +0900

    Task対応

--- app/controllers/satellites_controller.rb
+++ app/controllers/satellites_controller.rb
@@ -1,13 +1,20 @@
 class SatellitesController < ApplicationController
   require 'open-uri'
+  require 'nokogiri'
   def index
     @satellites = Satellite.all
-    @parsed_html = open("http://science.nasa.gov/iSat/iSAT-text-only/")
-    p @parsed_html
+    #html = Nokogiri::HTML(open("http://www.satellite-calculations.com/Satellite/satellitelist.php"))
+    #html.css('#main').css("tr")[20..-2].each_with_index do |tr, i|
+    #p tr.css("td")[1].text
+    # p tr.css("td")[0].text
+    # p tr.css("td")[7].text
+    #  p ""
+    end
   end
 
   def show
     @satellite = Satellite.find_by(name: params[:satellitename])
     @users = @satellite.users.all
   end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 class TasksController < ApplicationController
   def create
-    @satellite = Satellite.find_by name: params[:satellitename]
+    @satellite = Satellite.find_by name: params[:SatelliteName]
     @user = User.find_by(access_token: params[:access_token]).update(satellite_id: @satellite.id)
-    params[:tasks].each do |task|
-      @user.tasks.create(content: task, start_time: Time.now, user_id: @user.id)
+    @user.tasks.create(content: params[:task], start_time: Time.now, user_id: @user.id)
     end
   end
 end
