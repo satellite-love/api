75bb74d8db990d5f6e4b05d0dfe09b4e8ef0278f
regonn <regonn314@gmail.com>
2014-04-13 12:50:53 +0900

    endが多いバグを直す

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,5 @@ class TasksController < ApplicationController
     @satellite = Satellite.find_by name: params[:SatelliteName]
     @user = User.find_by(access_token: params[:access_token]).update(satellite_id: @satellite.id)
     @user.tasks.create(content: params[:task], start_time: Time.now, user_id: @user.id)
-    end
   end
 end
